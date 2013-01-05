6f5d92fb291792090e7fd5b1c88eb1ef917061af
OCox1991 <owen180291@googlemail.com>
2013-01-05 19:16:20 +0000

    v05/01/2013
    -Added Gene class
    -Added Cell class
    -Added basic breeding functionality to Gene
    -Added dominant recessive calculator to Cell

---
+++ Genome/Genome/Genome/Genes/Cell.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Genome
+{
+    class Cell
+    {
+        int dominantColour;
+        int nonDominantColour;
+
+        /// <summary>
+        /// Initialises the cell and selects which part will be the dominant and which part will be the non dominant colours
+        /// </summary>
+        /// <param name="colour1">A colour for the cell in the form of an int</param>
+        /// <param name="colour2">Another colour for the cell in the form of an int</param>
+        public Cell(int colour1, int colour2)
+        {
+            if (colour1 == colour2) //this could be gotten rid of if needed, but this has a better best case efficiency
+            {
+                dominantColour = colour1;
+                nonDominantColour = colour2;
+            }
+            else
+            {
+                Boolean oneIsDominant = false;
+                for (int i = 1; i < 4; i++) //check if colour2 is in the next 3 numbers after colour 1
+                {
+                    if(((colour1 - 1 + i)%7) + 1 == colour2) //the -1 next to colours makes it 0 to 6. Then the mod7 will also get 0 to 6. +1 is added since the colours we are actually using are 1 to 7
+                    {
+                        oneIsDominant = true; //so if colour1 + [1|2|3] is equal to colour2 then colour2 is dominated by colour1, otherwise it isn't.
+                    }
+                }
+                if (oneIsDominant)
+                {
+                    dominantColour = colour1;
+                    nonDominantColour = colour2;
+                }
+            }
+        }
+
+        #region Methods
+        /// <summary>
+        /// Gets the dominant colour of the cell, used when drawing the cell
+        /// </summary>
+        /// <returns>The dominant colour of the cell</returns>
+        public int getDomColour()
+        {
+            return dominantColour;
+        }
+
+        /// <summary>
+        /// Gets a random part of the cell, with the odds skewed towards the dominant part.
+        /// </summary>
+        /// <returns>A random part of the cell</returns>
+        public int getRandomPart()
+        {
+            Random r = new Random();
+            int col = -1;
+            int prob = r.Next(100) + 1;
+            if(prob <=60) //VARIABLE
+            {
+                col = dominantColour;
+            }
+            else
+            {
+                col = nonDominantColour;
+            }
+            return col;
+        }
+        #endregion
+    }
+}
---
+++ Genome/Genome/Genome/Genes/Gene.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Genome
+{
+    class Gene
+    {
+        Cell[][] cells;
+        Shape[] recognisedShapes;
+
+        /// <summary>
+        /// The empty constructor for the gene, which produces a random array of cells.
+        /// </summary>
+        public Gene()
+        {
+            cells = new Cell[10][]; //Using jagged arrays here to improve extensibility
+            Random r = new Random();
+            for(int i = 1; i < cells.Length; i++)
+            {
+                cells[i] = new Cell[10];
+                for(int j = 1; j < cells[i].Length; j++)
+                {
+                    cells[i][j] = new Cell(r.Next(7), r.Next(7));
+                }
+            }
+            this.init();
+        }
+
+        /// <summary>
+        /// An overloaded constructor for the gene that takes an array of cells. Used when breeding.
+        /// </summary>
+        /// <param name="cells">A 2D array of cells that represent this Genome</param>
+        public Gene(Cell[][] cells)
+        {
+            this.cells = cells;
+            this.init();
+        }
+
+        /// <summary>
+        /// init handles the initialisation code that is shared between the different constructors
+        /// </summary>
+        private void init()
+        {
+            recognisedShapes = Simulation.getShapes();
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// A method to breed this gene with another gene
+        /// </summary>
+        /// <param name="otherGene">The gene to breed this gene with</param>
+        /// <returns>The result of breeding these two genes together</returns>
+        public Gene breedWith(Gene otherGene)
+        {
+            Cell[][] newCells = new Cell[10][];
+            for (int i = 0; i < cells.Length; i++)
+            {
+                newCells[i] = new Cell[10];
+                for (int j = 0; j < cells.Length; j++)
+                {
+                    int colour1 = this.getPart(i,j);
+                    int colour2 = otherGene.getPart(i,j);
+                    newCells[i][j] = new Cell(colour1, colour2);
+                }
+            }
+            return new Gene(newCells);
+        }
+
+        /// <summary>
+        /// Gets a random part of a given cell
+        /// </summary>
+        /// <param name="row">the row of the cell to look at</param>
+        /// <param name="col">the column of the cell to look at</param>
+        /// <returns>a random part of the specified cell</returns>
+        public int getPart(int row, int col)
+        {
+            return cells[row][col].getRandomPart();
+        }
+
+        #endregion
+    }
+}
